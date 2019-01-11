61882ee9c6260232515c659b97d3e950ced62a31
Michael Klier <github@m-klier.de>
2019-01-11 10:15:17 +0100

    code cleanup

--- src/components/BaseEditorIconPicker.vue
+++ src/components/BaseEditorIconPicker.vue
@@ -4,15 +4,7 @@
 
 <script>
 import { fontawesomeiconpicker } from 'fontawesome-iconpicker'
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
 import $ from 'jquery'
-
-//library.add(fas, far, fab)
-
-//dom.watch()
 
 export default {
   props: ['toggle'],
@@ -25,7 +17,6 @@ export default {
       }
     })
     $(this.$el).on('iconpickerSelected', function(event) {
-      console.log(event.iconpickerValue)
       let f = event.iconpickerValue.substr(0, 3)
       let i = event.iconpickerValue.substr(7)
       let icon = [f, i]
